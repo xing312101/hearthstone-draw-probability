4ff7f61e2c07f44ebaf641e556b7a5962af4684c
xing312101 <company312101@gmail.com>
2018-11-09 00:55:21 +0800

    xing package

---
+++ xing/hearthstone_draw_probability.py
@@ -0,0 +1,15 @@
+import xing.xing_math as xing_math
+
+class hearthstone_draw_probability:
+    pass
+
+    def __init__(self):
+        pass
+
+    def starting_hand_probability(self):
+        pass
+
+    def round_draw_probability(self):
+        pass
+
+
